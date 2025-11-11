2473dadd25090228ea0b07e68c750359790a0975
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2025-11-11 15:28:43 -0800

    Add files via upload

---
+++ styles/CountyBoundary_2_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_CountyBoundary_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("name") !== null) {
+        labelText = String(feature.get("name"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(46,26,74,0.552)', lineDash: [4.9399999999999995,0.988], lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(14,79,208,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
